6bbbd6066f3613278a60c443f7c79a4fdc5ad907
Jeffrey Ning <jkcn90@outlook.com>
2015-10-10 00:43:11 -0400

    changed ternary search tree implementation, using counts for central node instead of nodes

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -1,24 +1,23 @@
 from collections import defaultdict
+import ipdb
 
 class Node:
 
-    def __init__(self, token, is_end_of_word=False):
+    def __init__(self, token):
         self.token = token
-        self.is_end_of_word = is_end_of_word
-
         self.left = None
-        self.center = None
         self.right = None
-        self.child_tokens = set()
+
+        self.child_tokens_dict = defaultdict(set)
+        self.is_end_of_word_dict = {}
 
     def __repr__(self):
-        return 'Node({}, is_end_of_word={})\n'.format(self.token, self.is_end_of_word)
+        return 'Node({})'.format(self.token)
 
     def __str__(self):
         left = self.left.token if self.left is not None else None
-        center = self.center.token if self.center is not None else None
         right = self.right.token if self.right is not None else None
-        return self.__repr__() + '({}, {}, {})'.format(left, center, right)
+        return self.__repr__() + ': ({}, {})'.format(left, right)
 
 
 class TernarySearchTree:
@@ -33,101 +32,122 @@ class TernarySearchTree:
         key = tokens[0]
         self.root_map[key] = add(tokens, self.root_map[key])
 
-    def search(self, tokens):
+    def search(self, tokens, flexible=True):
         key = tokens[0]
-        return search(tokens, self.root_map[key])
+        node, repeated_count = search(tokens, self.root_map[key], flexible)
+        return (node, repeated_count)
 
     def contains(self, tokens):
         if not tokens:
             return True
 
-        node = self.search(tokens)
+        (node, count) = self.search(tokens, flexible=False)
         if node is None:
             return False
-        if node.is_end_of_word:
-            return True
-        return False
+
+        last_token_count = 0
+        for token in reversed(tokens):
+            if token == tokens[-1]:
+                last_token_count += 1
+            else:
+                break
+        return count == last_token_count
 
     def get_completions(self, tokens):
         if not tokens:
             return []
 
-        node = self.search(tokens)
+        (node, count) = self.search(tokens)
         if node is None:
             return []
-        return get_completions(tokens, node)
+        return get_completions(tokens, node, starting_count=count-1)
 
     def __repr__(self):
         return self.root_map.keys().__repr__()
 
 def add(tokens, node):
     token = tokens[0]
-
     if node is None:
-        node = Node(token, is_end_of_word=False)
-        return add(tokens, node)
+        node = Node(token)
 
     if token > node.token:
         node.right = add(tokens, node.right)
+        return node
     elif token < node.token:
         node.left = add(tokens, node.left)
-    elif len(tokens) == 1:
-        node.is_end_of_word = True
-    else:
-        node.child_tokens.add(tokens[1])
-        if tokens[1] == node.token:
-            node.center = add(tokens[1:], node.center)
-        else:
-            return add(tokens[1:], node)
-    return node
-
-def search(tokens, node):
-    if not node:
-        return
-    if not tokens:
         return node
 
-    if tokens[0] == node.token:
-        tokens = tokens[1:]
-        if not tokens:
-            return node
+    for i, token in enumerate(tokens):
+        if token != node.token:
+            node.child_tokens_dict[i].add(token)
+            return add(tokens[i:], node)
+    node.child_tokens_dict[len(tokens)].add('')
+    node.is_end_of_word_dict[len(tokens)] = True
+    return node
 
-    right_search = search(tokens, node.right)
+def search(tokens, node, flexible=True):
+    if node is None:
+        return (None, 0)
+
+    this_token_count = 0
+    for token in tokens:
+        if token == node.token:
+            this_token_count += 1
+        else:
+            break
+
+    reduction = 0
+    for token_count in node.child_tokens_dict:
+        if flexible and (reduction < token_count) and (this_token_count <= token_count):
+            reduction = token_count
+        if (not flexible) and (this_token_count == token_count):
+            reduction = this_token_count
+
+    if flexible:
+        reduction = min(reduction, this_token_count)
+    tokens = tokens[reduction:]
+
+    if not tokens:
+        return (node, reduction)
+
+    right_search, repeated_count = search(tokens, node.right, flexible)
     if right_search is not None:
-        return right_search
+        return (right_search, repeated_count)
 
-    left_search = search(tokens, node.left)
+    left_search, repeated_count = search(tokens, node.left, flexible)
     if left_search is not None:
-        return left_search
+        return (left_search, repeated_count)
+    return (None, 0)
 
-    center_search = search(tokens, node.center)
-    if center_search is not None:
-        return center_search
-
-def get_completions(tokens, node, target_token=None):
+def get_completions(tokens, node, target_token=None, starting_count=0):
     if node is None:
         return []
 
     get_new_target_tokens = (target_token is None) or (node.token == target_token)
 
     if get_new_target_tokens:
-        target_tokens = [token for token in node.child_tokens]
+        target_token_counts = [(count, value)
+                               for count, value_set
+                               in node.child_tokens_dict.items()
+                               for value in list(value_set)]
     else:
-        target_tokens = [target_token]
+        target_token_counts = [(0, target_token)]
 
     completions = []
-    for target_token in target_tokens:
+    for count, target_token in target_token_counts:
+        if starting_count:
+            count -= starting_count
+        count = max(count - 1, 0)
+
         if get_new_target_tokens:
-            new_tokens = tokens + target_token
+            new_tokens = tokens + count * node.token + target_token
         else:
             new_tokens = tokens
 
         if target_token > node.token:
             completions += get_completions(new_tokens, node.right, target_token)
-        elif target_token < node.token:
+        if target_token < node.token:
             completions += get_completions(new_tokens, node.left, target_token)
-        else:
-            completions += get_completions(new_tokens, node.center, target_token)
-    if node.is_end_of_word:
-        completions += [tokens]
+        if target_token == '':
+            completions += [new_tokens]
     return list(set(completions))
--- tests/test_ternary_search_tree.py
+++ tests/test_ternary_search_tree.py
@@ -4,27 +4,14 @@ from ternary_search_tree import Node, TernarySearchTree
 class TestNode(unittest.TestCase):
 
     def test_initialization(self):
-        node = Node('a', False)
+        node = Node('a')
         self.assertEqual(node.token, 'a')
-        self.assertEqual(node.child_tokens, set())
+        self.assertEqual(node.child_tokens_dict['A'], set())
         self.assertEqual(node.left, None)
-        self.assertEqual(node.center, None)
         self.assertEqual(node.right, None)
-        self.assertFalse(node.is_end_of_word)
-
-        node = Node('b', True)
-        self.assertEqual(node.token, 'b')
-        self.assertTrue(node.is_end_of_word)
-
-    def test_default_initialization(self):
-        node = Node('c')
-        self.assertEqual(node.token, 'c')
-        self.assertFalse(node.is_end_of_word)
+        self.assertEqual(node.is_end_of_word_dict, {})
 
 class TestTernarySearchTree(unittest.TestCase):
-
-    def test_initialization(self):
-        tree = TernarySearchTree()
 
     def test_add_one_token(self):
         tree = TernarySearchTree()
@@ -32,7 +19,15 @@ class TestTernarySearchTree(unittest.TestCase):
         node = tree.root_map['a']
         self.assertEqual(node.token, 'a')
 
-    def test_add_one_word(self):
+    def test_add_one_repeating_token(self):
+        tree = TernarySearchTree()
+        tree.add('aattaa')
+        node = tree.root_map['a']
+        self.assertEqual(node.token, 'a')
+        self.assertEqual(node.right.child_tokens_dict[2], {'a'})
+        self.assertEqual(node.right.left.child_tokens_dict[2], {''})
+
+    def test_add_one_sentence(self):
         tree = TernarySearchTree()
         tree.add('An aapple fell off the tree')
         node = tree.root_map['A']
@@ -40,55 +35,66 @@ class TestTernarySearchTree(unittest.TestCase):
         self.assertEqual(node.right.token, 'n')
         self.assertEqual(node.right.left.token, ' ')
         self.assertEqual(node.right.left.right.token, 'a')
-        self.assertEqual(node.right.left.right.center.token, 'a')
 
-    def test_add_two_words(self):
+    def test_add_two_sentences(self):
         tree = TernarySearchTree()
         tree.add('An aapple fell off the tree')
         tree.add('An aardvark fell off the tree')
-        node = tree.root_map['A'].right.left.right.center
+        node = tree.root_map['A'].right.left.right
         self.assertEqual(node.right.token, 'p')
         self.assertEqual(node.right.right.token, 'r')
         self.assertEqual(node.right.right.token, 'r')
-        self.assertEqual(node.right.center.token, 'p')
-        self.assertEqual(node.right.center.left.token, 'l')
+        self.assertEqual(node.right.token, 'p')
+        self.assertEqual(node.right.left.token, 'l')
 
     def test_search_single_token(self):
         tree = TernarySearchTree()
         tree.add('a')
-        node = tree.search('a')
+        node, count = tree.search('a')
         self.assertEqual(node.token, 'a')
+        self.assertEqual(count, 1)
+
+    def test_search_repeated_token(self):
+        tree = TernarySearchTree()
+        tree.add('aaaa')
+        node, count = tree.search('aaa')
+        self.assertEqual(node.token, 'a')
+        self.assertEqual(count, 3)
 
     def test_search_not_in_tree(self):
         tree = TernarySearchTree()
         tree.add('a')
-        node = tree.search('b')
+        node, count = tree.search('b')
         self.assertEqual(node, None)
+        self.assertEqual(count, 0)
 
     def test_search(self):
         tree = TernarySearchTree()
         tree.add('An aapple fell off the tree')
         tree.add('An aardvark fell off the tree')
 
-        node = tree.search('An aa')
+        (node, count) = tree.search('An aa')
         self.assertEqual(node.token, 'a')
-        self.assertFalse(node.is_end_of_word)
+        self.assertEqual(count, 2)
 
-        node = tree.search('An aardvark')
+        (node, count) = tree.search('An aardvark')
         self.assertEqual(node.token, 'k')
-        self.assertFalse(node.is_end_of_word)
+        self.assertEqual(count, 1)
 
-        node = tree.search('An aaple fell off the tree')
+        (node, count) = tree.search('An aapple fell off the tree')
         self.assertEqual(node.token, 'e')
-        self.assertTrue(node.is_end_of_word)
+        self.assertEqual(count, 2)
 
     def test_contains(self):
         tree = TernarySearchTree()
         tree.add('An aapple fell off the tree')
         tree.add('An aardvark fell off the tree')
 
-        result = tree.contains('An aaple fell off the tree')
+        result = tree.contains('An aapple fell off the tree')
         self.assertTrue(result)
+
+        result = tree.contains('An aaple fell off the tree')
+        self.assertFalse(result)
 
         result = tree.contains('An aardvark fell off the tree')
         self.assertTrue(result)
@@ -106,7 +112,7 @@ class TestTernarySearchTree(unittest.TestCase):
         tree.add('An aardvark fell off the tree')
         completions = tree.get_completions('An aa')
         expected = set(['An aapple fell off the tree',
-                    'An aardvark fell off the tree'])
+                        'An aardvark fell off the tree'])
         self.assertEqual(set(completions), expected)
 
     def test_get_completions_words(self):
@@ -114,13 +120,22 @@ class TestTernarySearchTree(unittest.TestCase):
         sentences = ['An aapple fell off the tree',
                      'An aardvark fell off the tree',
                      'asdf',
-                     'asdf airplane asdfpppwpi, gjas',
+                     'asdf airplane asdfpppwpi gjas',
                     ]
         for sentence in sentences:
             for word in sentence.split():
                 tree.add(word)
         completions = tree.get_completions('a')
-        expected = set(['aapple', 'aardvark'])
+        expected = set(['aapple', 'aardvark', 'asdfpppwpi', 'airplane', 'asdf'])
+        self.assertEqual(set(completions), expected)
+
+    def test_get_repeated_completions(self):
+        tree = TernarySearchTree()
+        tree.add('An aaaaaaaaa')
+        tree.add('An aaaaaaaab')
+        completions = tree.get_completions('An aa')
+        expected = set(['An aaaaaaaaa',
+                        'An aaaaaaaab'])
         self.assertEqual(set(completions), expected)
 
 
