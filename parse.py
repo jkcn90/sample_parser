7a553fc28a41eb2261aaf68953214ff3f48039f6
Jeffrey Ning <jkcn90@outlook.com>
2015-10-12 02:23:11 -0400

    testing predictions using clustering

--- parse.py
+++ parse.py
@@ -1,7 +1,14 @@
 import os
 import sys
 import json
-from itertools import permutations
+import numpy as np
+
+from sklearn import metrics
+from sklearn.pipeline import make_pipeline
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import Normalizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import sample_parser
 from sample_parser.ternary_search_tree import TernarySearchTree as SearchTree
@@ -19,12 +26,20 @@ words = [message.split() for message in messages]
 words = [word for word_list in words for word in word_list]
 words = list(set(words))
 
-word_tree = SearchTree()
-for word in words:
-    word_tree.add(word)
-    
-sentences = [message.split() for message in messages]
+vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 3),
+                             stop_words='english',
+                             max_df=0.01, min_df=0.001)
+X = vectorizer.fit_transform(messages)
 
-sentence_tree = SearchTree()
-for sentence in sentences:
-    sentence_tree.add(sentence)
+n_components = 500
+svd = TruncatedSVD(n_components)
+lsa = make_pipeline(svd, Normalizer(copy=False))
+
+X = lsa.fit_transform(X)
+
+n_clusters = 30
+
+km = MiniBatchKMeans(n_clusters=n_clusters, init='k-means++', n_init=1,
+                             init_size=1000, batch_size=1000)
+km.fit(X)
+message_predictions = km.predict(X)
