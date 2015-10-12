424ae503c5d06b655d0f9c1607a018c7df5fff5f
Jeffrey Ning <jkcn90@outlook.com>
2015-10-12 06:17:51 -0400

    more parameter tuning and added sentence autocompletion back in

--- parse.py
+++ parse.py
@@ -11,6 +11,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 import sample_parser
 from sample_parser.ternary_search_tree import TernarySearchTree as SearchTree
+sys.setrecursionlimit(10000)
 
 data_file_path = "data/sample_conversations.json"
 data_file_path = os.path.join(sample_parser.__path__[0], data_file_path)
@@ -21,16 +22,16 @@ messages = [message['Text']
             for datum in data['Issues'] 
             for message in datum['Messages']]
 
-words = [message.split() for message in messages]
-words = [word for word_list in words for word in word_list]
+sentences = [message.split() for message in messages]
+words = [word for word_list in sentences for word in word_list]
 words = list(set(words))
 
 vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 3),
                              stop_words='english',
-                             max_df=0.008, min_df=0.0009)
+                             max_df=0.007, min_df=0.001)
 X = vectorizer.fit_transform(messages)
 
-n_clusters = 1000
+n_clusters = 1500
 
 km = MiniBatchKMeans(n_clusters=n_clusters, init='k-means++', n_init=1,
                              init_size=10000, batch_size=5000)
@@ -40,3 +41,7 @@ message_predictions = km.predict(X)
 word_tree = SearchTree()
 for word in words:
     word_tree.add(word)
+
+sentence_tree = SearchTree()
+for message in messages:
+    sentence_tree.add(message)
